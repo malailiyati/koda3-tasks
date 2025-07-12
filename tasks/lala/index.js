1d12b7fd4c4a2842fb2777aff60a8c28e19ec870
unknown <malailiyati107@gmail.com>
2025-07-12 17:25:07 +0700

    feat : import dan eksport fungsi

---
+++ tasks/lala/index.js
@@ -0,0 +1,16 @@
+const nosatu = require("./nosatu");
+const nodua = require("./nodua");
+const notiga = require("./notiga");
+const noempat = require("./noempat");
+const nolima = require("./nolima");
+const noenam = require("./noenam");
+
+
+module.exports = {
+    triangle : nosatu.triangle,
+    countArray : nodua.countArray,
+    triangleIsiTerbalik : notiga.triangleIsiTerbalik,
+    processNumber : noempat.processNumber,
+    cetakTabel : nolima.cetakTabel,
+    buatRentang : noenam.buatRentang
+};
