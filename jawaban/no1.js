5c52f298ccc4ec0fb56ae515ece74e82f996f692
unknown <malailiyati107@gmail.com>
2025-07-12 14:01:33 +0700

    feat : menambahkan file jawaban no 1 triangle

--- jawaban/no1.js
+++ jawaban/no1.js
@@ -1,12 +1,14 @@
 function triangle(num) {
   if (typeof num === "number" && num >= 0) {
+    let result = ""
     for (let i = 1; i <= num; i++) {
       let line = " ";
       for (let j = 1; j <= i; j++) {
         line += j + " ";
       }
-      console.log(line);
+      result += line + "\n";
     }
+    return result
   } else {
     console.log("paramenter harus bertipe data nomor dan harus positif");
   }
