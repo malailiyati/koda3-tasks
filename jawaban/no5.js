eb87103e03e175da14177e6f5ae5a0a20b728bf7
unknown <malailiyati107@gmail.com>
2025-07-12 14:02:36 +0700

    feat : menambahkan file jawaban no 5 triangle

--- jawaban/no5.js
+++ jawaban/no5.js
@@ -1,13 +1,15 @@
 function cetakTabel(n) {
   if (typeof n === "number" && n >= 0) {
+    let result = "";
     let hasil = 0;
     for (let i = 1; i <= 10; i++) {
       hasil = n * i;
       let line = " ";
       line += n + " x " + i + " = " + hasil;
 
-      console.log(line);
+      result += line + "\n";
     }
+    return result;
   } else {
     console.log("paramenter harus bertipe data nomor dan harus positif");
   }
