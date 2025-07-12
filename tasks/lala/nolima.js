1bf6df4282d6747cad3e18560e38831cdf391d88
unknown <malailiyati107@gmail.com>
2025-07-12 17:21:39 +0700

    feat : membuat function tabel perkalian

---
+++ tasks/lala/nolima.js
@@ -0,0 +1,18 @@
+function cetakTabel(n) {
+    if (typeof n === "number" && n >= 0) {
+        let result = "";
+        let hasil = 0;
+        for (let i = 1; i <= 10; i++) {
+            hasil = n * i;
+            let line = " ";
+            line += n + " x " + i + " = " + hasil;
+
+            result += line + "\n";
+        }
+        return result;
+    } else {
+        throw new Error("paramenter harus bertipe data nomor dan harus positif");
+    }
+}
+
+module.exports = {cetakTabel};
