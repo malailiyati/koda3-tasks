48d996f0308ec975a84130ea52a93448f78f4290
unknown <malailiyati107@gmail.com>
2025-07-12 17:21:06 +0700

    feat : membuat function yang menghitung perkalian bil ganjil arr

---
+++ tasks/lala/noempat.js
@@ -0,0 +1,12 @@
+function processNumber(arr) {
+    let sum = 1;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            sum *= arr[i];
+        }
+    }
+    return sum;
+}
+
+module.exports = {processNumber};
