b8c2c3dc39dc4789d1d8317b900b1642727d6f3b
unknown <malailiyati107@gmail.com>
2025-07-12 17:19:06 +0700

    feat : membuat function triangle

---
+++ tasks/lala/nosatu.js
@@ -0,0 +1,17 @@
+function triangle(num) {
+    if (typeof num === "number" && num >= 0) {
+        let result = "";
+        for (let i = 1; i <= num; i++) {
+            let line = " ";
+            for (let j = 1; j <= i; j++) {
+                line += j + " ";
+            }
+            result += line + "\n";
+        }
+        return result;
+    } else {
+        throw new Error("paramenter harus bertipe data nomor dan harus positif");
+    }
+}
+
+module.exports = {triangle};
