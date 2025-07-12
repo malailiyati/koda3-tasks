ad41222f3595129052c477aee79de6ff9f69f85c
unknown <malailiyati107@gmail.com>
2025-07-12 13:04:27 +0700

    feat : import file-file baru

--- index.js
+++ index.js
@@ -26,45 +26,43 @@ buatRentang(5, 10); //no6
 
 const irul = require("./tasks/irul");
 
-function tasksIrul() {
-  // Program: Sum Number
-  // Success
-  const resultSum = irul.sumNumber(10, 50);
-  console.log(resultSum);
+function tasksIrul(){
+    // Program: Sum Number
+    // Success
+    const resultSum = irul.sumNumber(10,50);
+    console.log(resultSum);
 
-  // Failed
-  try {
-    const result = irul.sumNumber("10", 30);
-    console.log(result);
-  } catch (error) {
-    console.log(error.message);
-  }
-  // Program: Multiply Number
-  // Success
-  const resultMultiply = irul.multiplyNumbers(10, 50);
-  console.log(resultMultiply);
+    // Failed
+    try{
+        const result = irul.sumNumber("10",30);
+        console.log(result);
+    }catch(error){
+        console.log(error.message);
+    }
+    // Program: Multiply Number
+    // Success
+    const resultMultiply = irul.multiplyNumbers(10,50);
+    console.log(resultMultiply);
 
-  // Failed
-  try {
-    const result = irul.multiplyNumbers("10", 30);
-    console.log(result);
-  } catch (error) {
-    console.log(error.message);
-  }
-  // Program: Convert Celcius to Fahrenheit
-  // Success
-  const temperatureResult = irul.celciusToFahrenheit(10);
-  console.log(
-    `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
-  );
+    // Failed
+    try{
+        const result = irul.multiplyNumbers("10",30);
+        console.log(result);
+    }catch(error){
+        console.log(error.message);
+    }
+    // Program: Convert Celcius to Fahrenheit
+    // Success
+    const temperatureResult = irul.celciusToFahrenheit(10);
+    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
 
-  // Failed
-  try {
-    const result = irul.celciusToFahrenheit("10", 30);
-    console.log(result);
-  } catch (error) {
-    console.log(error.message);
-  }
+    // Failed
+    try{
+        const result = irul.celciusToFahrenheit("10",30);
+        console.log(result);
+    }catch(error){
+        console.log(error.message);
+    }
 }
 tasksIrul();
 
