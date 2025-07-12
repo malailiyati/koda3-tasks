31e34c1efe5a4defd55b56cecf7233fc302a6752
unknown <malailiyati107@gmail.com>
2025-07-12 17:20:39 +0700

    feat : membuat function triangle isi terbalik

---
+++ tasks/lala/notiga.js
@@ -0,0 +1,17 @@
+function triangleIsiTerbalik(num) {
+    if (typeof num === "number" && num >= 0) {
+        let result = "";
+        for (let i = 1; i <= num; i++) {
+            let line = " ";
+            for (let j = i; j >= 1; j--) {
+                line += j + " ";
+            }
+            result += line + "\n";
+        }
+        return result;
+    } else {
+        throw new Error("paramenter harus bertipe data nomor dan harus positif");
+    }
+}
+
+module.exports = {triangleIsiTerbalik};
